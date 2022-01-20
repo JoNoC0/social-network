4e9bcc8072d17a341433ed190f3764d3f71529c6
JoNoC0 <jnguyeningco@gmail.com>
2022-01-20 13:19:45 -0600

    added Reaction to Thought.js

--- models/Thought.js
+++ models/Thought.js
@@ -1,27 +1,58 @@
-const { Schema, models } = ('mongoose');
+const { Schema, model, Types } = ('mongoose');
+const moment = require('moment');
+
+// ReactionSchema
+const ReactionSchema = new Schema({
+    // set custom Id
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: ()=> new Types.ObjectId()
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (createdAtVal) => moment(createdAtVal).format('MMM Do, YYYY [at] hh:mm a')
+    },
+    toJson: {
+        getters: true
+    }
+    }
+);
 
 const ThoughtSchema = new Schema({
     thoughtText: {
         type: String,
         required: true,
-        minlength: 280
+        minlength: 1,
+        maxlength: 280
     },
     createdAt: {
         type: Date,
         default: Date.now,
-        get: createdAtVal => dateFormat(createdAtVal)
+        get: createdAtVal => moment(createdAtVal).format('MMM Do, YYYY [at] hh:mm a')
     },
     username: {
         type: String,
         required: true
     },
     // use reaction schema to validate data
-    reactions:  [ReactionSchema],
-        toJson: {
-            virtuals: true,
-            getters: true
-        },
-        id: false
+    reactions:  [ReactionSchema]
+    },
+    {
+    toJson: {
+        virtuals: true,
+        getters: true
+    },
+    id: false
     }
 )
 
