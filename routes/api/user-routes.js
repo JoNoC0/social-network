003d84c9597f3091e2508a8ba666099d8a54d53b
JoNoC0 <jnguyeningco@gmail.com>
2022-01-20 18:15:07 -0600

    user-routes.js

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -11,3 +11,22 @@ const {
     addFriend,
     deleteFriend
 } = require('../../controllers/user-controller');
+
+// GET, POST to /api/users
+router.route('/')
+    .get(getAllUser)
+    .post(createUser);
+
+// GET, PUT, DELETE /api/users/:id
+router.route('/:id')
+    .get(getUserById)
+    .put(updateUser)
+    .delete(deleteUser);
+
+// POST, DELETE /api/users/:userID/friends/:friendId
+router.route('/:id/friends/:friendId')
+    .post(addFriend)
+    .delete(deleteFriend);
+
+// module export router
+module.exports = router;
